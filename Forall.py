5d47d2420889714404410981052c902417896da8
JRGIL12 <108557695+JRGIL12@users.noreply.github.com>
2024-07-13 15:39:37 -0400

    forall intento2

--- Forall.py
+++ Forall.py
@@ -5,22 +5,23 @@ def complejidad (num):
         return 1
     
     if num % 11 == 0:
-        return 1 + complejidad(num // 11)
+        return 1 +1+ complejidad(num // 11)
 
     if num > 11:
         if num % 10 == 0:
-            return 1 + complejidad(num % 11)
+            return 1+1 + complejidad(num // 11) + complejidad(num % 11)
         return 1 +complejidad(num % 10)
-    
-    if num == 2:
-        return 2
-    if num % 2 == 0:
-        return 2 + complejidad(num//2)
     
     if num == 3:
         return 3
     if num % 3 == 0:
         return 3 + complejidad(num//3)
+
+    if num == 2:
+        return 2
+    if num % 2 == 0:
+        return 2 + complejidad(num//2)
+
     
     return 1 + complejidad(num-1)
 
