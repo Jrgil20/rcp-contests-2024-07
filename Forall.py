773602578545435ec021c5b3859ef972cfa0c871
JRGIL12 <108557695+JRGIL12@users.noreply.github.com>
2024-07-13 17:13:22 -0400

    forall intento 3

--- Forall.py
+++ Forall.py
@@ -1,32 +1,42 @@
-def complejidad (num):
+def complejidad(num):
     if num == 0:
         return 0
-    if num == 1:
-        return 1
-    
-    if num % 11 == 0:
-        return 1 +1+ complejidad(num // 11)
-
-    if num > 11:
-        if num % 10 == 0:
-            return 1+1 + complejidad(num // 11) + complejidad(num % 11)
-        return 1 +complejidad(num % 10)
-    
-    if num == 3:
+    elif num > 11111:
+        if num // 11111:
+            return 5 + complejidad(num // 11111) + complejidad(num % 11111)
+        else:
+            return 5 + complejidad(num % 11111)
+    elif num > 1111:
+        if num // 1111 != 1:
+            return 4 + complejidad(num // 1111) + complejidad(num % 1111)
+        else:
+            return 4 + complejidad(num % 1111)
+    elif num > 111:
+        if num //111 != 1:           
+            return 3 + complejidad(num // 111) + complejidad(num % 111)
+        else:
+            return 3 + complejidad(num % 111)
+    elif num > 11:
+        if num // 11 != 1:
+            return 2 + complejidad(num //11 ) + complejidad(num % 11)
+        else:
+            return 2 + complejidad(num % 11)
+    elif num == 3:
         return 3
-    if num % 3 == 0:
-        return 3 + complejidad(num//3)
-
-    if num == 2:
+    elif num % 3 == 0:
+        return complejidad(num // 3) +3
+    elif num == 2:
         return 2
-    if num % 2 == 0:
-        return 2 + complejidad(num//2)
+    elif num % 2 == 0:
+        return  complejidad(num // 2) +2
+    elif num == 1:
+        return 1
+    else:
+        return 1 + complejidad(num - 1)
 
-    
-    return 1 + complejidad(num-1)
+
 
 num = int(input())
-if num > 0 or num < 1000000:
+if 0 < num < 100000:
     result = complejidad(num)
-print(result)
-
+    print(result)
